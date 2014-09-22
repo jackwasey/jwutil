0d7fc7496d99e65d0c9e3425fb187269195c485d
Jack Wasey <jack@jackwasey.com>
2014-09-21 20:52:18 -0400

    doc

--- R/jwutil-package.r
+++ R/jwutil-package.r
@@ -1,5 +1,7 @@
-#' jwutil.
-#'
+#' @title jwutil.
+#' @description A small set of simple utilities for common data manipulation tasks,
+#' primarily of data frames. The goal is typically to use built-in tools well,
+#' without bringing in huge dependencies.
 #' @name jwutil
 #' @docType package
 NULL
---
+++ README.md
@@ -0,0 +1,11 @@
+# jwutil
+
+A small set of simple utilities for common data manipulation tasks,
+primarily of data frames. The goal is typically to use built-in tools well,
+without bringing in huge dependencies.
+
+## install
+```
+library(devools)
+install_github("jackwasey/jwutil")
+```
