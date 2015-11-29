741ebd865e3d8ce40348e8bd76f88aafbaaedaa5
Jack Wasey <jack@jackwasey.com>
2015-11-29 15:05:52 -0500

    docs

--- R/jwutil-package.r
+++ R/jwutil-package.r
@@ -1,8 +1,7 @@
-#' @title jwutil.
-#' @description A small set of simple utilities for common data manipulation
-#'   tasks, primarily of data frames. The goal is typically to use built-in
-#'   tools well, without bringing in huge dependencies.
-#' @name jwutil
+#' @title Tools for data manipulation not found elsewhere, and testthat extensions
+"_PACKAGE"
+#' @name jwutil-package
+#' @aliases jwutil jwutil-package
 #' @useDynLib jwutil
 #' @docType package
 NULL
