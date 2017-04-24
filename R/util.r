0f0f6252d287bf53b722f89a61fe9b2917d7649c
Jack O. Wasey <jack@jackwasey.com>
2017-04-24 08:51:16 -0400

    move as_char_no_warn to own file

--- R/util.r
+++ R/util.r
@@ -45,23 +45,6 @@ allIsInteger <- function(x, tol =  1e-9, na.rm = TRUE)
     na.rm = na.rm
   )
 
-#' convert to character vector without warning
-#' @param x vector, typically numeric or a factor
-#' @return character vector
-#' @export
-as_char_no_warn <- function(x) {
-  if (is.character(x))
-    return(x)
-  old <- options(warn = -1)
-  on.exit(options(old))
-  if (is.integer(x))
-    fastIntToStringRcpp(x)
-  if (is.factor(x))
-    levels(x)[x]
-  else
-    as.character(x)
-}
-
 #' convert factor or vector to numeric without warnings
 #'
 #' correctly converts factors to vectors, and then converts to
