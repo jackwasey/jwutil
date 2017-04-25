957f138b6d792fc20802abfb930a351d86849e23
Jack O. Wasey <jack@jackwasey.com>
2017-04-25 15:23:17 -0400

    consolidate duplicated save_in_data_dir

--- R/file.r
+++ R/file.r
@@ -56,29 +56,34 @@ unzip_single <- function(url, file_name, save_path) {
   ret
 }
 
-
 #' Save given variable in package data directory
 #'
-#' File will be named \emph{var_name}\code{.RData} with an optional suffix
-#' before \code{.RData}
+#' File is named varname.RData with an optional suffix before .RData
 #'
-#' @param var_name character or symbol, e.g. \code{"myvar"} or \code{myvar},
-#'   either of which would find \code{myvar} in the parent environment, and save
-#'   it as \code{myvar.RData} in \code{package_root/data}.
+#' @param var_name character or symbol, e.g. "myvar" or \code{myvar}, either of
+#'   which would find \code{myvar} in the parent environment, and save it as
+#'   \code{myvar.RData} in \code{package_root/data}.
 #' @param suffix character scalar
 #' @param data_path path to data directory, default is data in current directory.
+#' @param package_dir character containing the directory root of the package
+#'   tree in which to save the data. Default is the current working directory.
+#' @param envir environment in which to look for the variable to save
 #' @return invisibly returns the data
-#' @keywords internal file
-save_in_data_dir <- function(var_name, suffix = "", data_path = "data") {
-  checkmate::assert_string(suffix)
+#' @export
+save_in_data_dir <- function(var_name, suffix = "", data_path = "data",
+                             package_dir = getwd(), envir = parent.frame()) {
+  checkmate::assertString(suffix)
+  stopifnot(exists(var_name, envir = envir))
   var_name <- as.character(substitute(var_name))
-  stopifnot(exists(var_name, envir = parent.frame()))
+  checkmate::assertString(var_name)
+  stopifnot(exists(var_name, envir = envir))
   save(list = var_name,
-       envir = parent.frame(),
-       file = file.path(data_path, strip(paste0(var_name, suffix, ".RData"))),
+       envir = envir,
+       file = file.path(package_dir, data_path,
+                        strip(paste0(var_name, suffix, ".RData"))),
        compress = "xz")
   message("Now reload package to enable updated/new data: ", var_name)
-  invisible(get(var_name, envir = parent.frame()))
+  invisible(get(var_name, envir = envir))
 }
 
 # tempdir optional hard stop, to make sure CRAN doesn't get polluted and throw
--- R/util.r
+++ R/util.r
@@ -592,31 +592,6 @@ source_purl <- function(input,
   source(output, ...)
 }
 
-#' Save given variable in package data directory
-#'
-#' File is named varname.RData with an optional suffix before .RData
-#'
-#' @param var_name character or symbol, e.g. "myvar" or \code{myvar}, either of
-#'   which would find \code{myvar} in the parent environment, and save it as
-#'   \code{myvar.RData} in \code{package_root/data}.
-#' @param suffix character scalar
-#' @param package_dir character containing the directory root of the package
-#'   tree in which to save the data. Default is the current working directory.
-#' @param envir environment in which to look for the variable to save
-#' @export
-save_in_data_dir <- function(var_name, suffix = "",
-                             package_dir = getwd(), envir = parent.frame()) {
-  checkmate::assertString(suffix)
-  var_name <- as.character(substitute(var_name))
-  checkmate::assertString(var_name)
-  stopifnot(exists(var_name, envir = envir))
-  save(list = var_name,
-       envir = envir,
-       file = file.path(package_dir, "data", strip(paste0(var_name, suffix, ".RData"))),
-       compress = "xz")
-  message("Now reload package to enable updated/new data: ", var_name)
-}
-
 utils::globalVariables(c(".", "%>%"))
 
 #' Find minimum R version required for package
