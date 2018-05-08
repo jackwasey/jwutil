2d0c0011c06faf77441b108d911e0021eab40c4b
Jack O. Wasey <jack@jackwasey.com>
2018-05-08 08:49:51 -0400

    checks zero problems

--- R/as_char_no_warn.r
+++ R/as_char_no_warn.r
@@ -1,6 +1,7 @@
 #' convert to character vector without warning
 #' @param x vector, typically numeric or a factor
 #' @return character vector
+#' @importFrom Rcpp evalCpp
 #' @export
 as_char_no_warn <- function(x) {
   if (is.character(x))
